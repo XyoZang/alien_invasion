3142a65bc3a3b89b39fae9e9fe2bd7882b1759d8
Riddel <_Riddel@ex.com_>
2021-10-07 20:15:56 +0800

    Version1.2.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,16 +23,19 @@ class AlienInvasion:
         """开始游戏的主循环"""
         while True:
             self._check_events()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitime()
-            # 让最近绘制的屏幕可见。
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """响应按键和鼠标事件。"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitime()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
