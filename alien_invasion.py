8a67652b09dfd0cda7995d6fe22e61d34e3ed525
Riddel <_Riddel@ex.com_>
2021-10-08 16:41:18 +0800

    Version2.3.1--ScoreBoard

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,6 +26,9 @@ class AlienInvasion:
 
         # 创建一个用于存储游戏统计信息的实例
         self.stats = GameStats(self)
+
+        # 创建记分牌
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -67,7 +71,7 @@ class AlienInvasion:
         if button_clicked and not self.stats.game_active:
             # 重置游戏设置
             self.settings.initialize_dynamic_settings()
-            
+
             # 重置游戏统计信息
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -219,6 +223,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # 显示得分
+        self.sb.show_score()
 
         # 如果游戏处于非活动状态，就绘制Play按钮
         if not self.stats.game_active:
