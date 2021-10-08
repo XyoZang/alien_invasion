524bfd28e7e8e171fdd45ee94326fd8289bc9e89
Riddel <_Riddel@ex.com_>
2021-10-08 18:49:13 +0800

    All_Done

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,6 +77,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # 清空余下的外星人和子弹
             self.aliens.empty()
@@ -212,8 +213,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """响应飞船被外星人撞到"""
         if self.stats.ships_left > 0:
-            # 将ships_left减 1
+            # 将ships_left减 1 并更新记分牌
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # 清空余下的外星人和子弹
             self.aliens.empty()
